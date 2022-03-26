ff7a65c1f90e5e80a51472b818d0d5d1d017a268
Alex McKinnon <alex@mckinnon.io>
2022-03-25 23:00:44 -0600

    change auth.py to config.py

--- getMetadata.py
+++ getMetadata.py
@@ -2,21 +2,50 @@
 
 import requests, json
 
-import auth
+import config
+
+# Welcome
+print("Welcome to the BooksAPI metadata retreiver\n")
+print("------\n")
 
 # Initiate Session:
+
+print("Logging in with "+config.user+" to "+config.baseURL)
 
 # Initiate & configure session
 session = requests.Session()
 
-session.auth(auth.user,auth.passwd)
+session.auth(config.user, config.passwd)
 session.headers.update({
 	"Content-Type":"application/json",
 	})
 
 # Retrieve list of series from Komga
 
+try:
+	response = session.get(config.baseURL+"/api/v1/series?size=10")
+	response.raise_for_status()
+	content = response.json()
+	print("The request was a success!")
 
+	print(type(response.json()))
+	print("\n ------ \n")
+	print(response.json().keys())
+
+	
+
+except requests.exceptions.HTTPError as errh:
+	print(errh)
+	sys.exit(1)
+except requests.exceptions.ConnectionError as errc:
+	print(errc)
+	sys.exit(1)
+except requests.exceptions.Timeout as errt:
+	print(errt)
+	sys.exit(1)
+except requests.exceptions.RequestException as err:
+	print(err)
+	sys.exit(1)
 
 # Loop to look up per library series
 
