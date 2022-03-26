5927b12b5d607081ac960de67df22c73eec8f9b2
Alex McKinnon <alex@mckinnon.io>
2022-03-25 22:04:26 -0600

    Update auth.template.py

--- auth.template.py
+++ auth.template.py
@@ -1,3 +1,6 @@
-# Enter credentials here
+# Enter server and credentials here
+
+
+baseURL="https://example.com" # Include protocol (ie. http:// or https://) and port (if relevant)
 user=""
 pass=""
