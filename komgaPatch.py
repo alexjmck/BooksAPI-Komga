e4f6fc8cd8c9c1f8f377ec56e3219741866c27cf
Alex McKinnon <alex@mckinnon.io>
2022-03-27 01:16:57 -0600

    pushJSON outline added

--- komgaPatch.py
+++ komgaPatch.py
@@ -1,4 +1,45 @@
 # Push to Komga
 
 def patchSeries(session, komgaSeries, aniListSeries): # pass requests session, Series and anilist class
+	
+
+
+
+
+
 	print(aniListSeries.description)
+
+	pushJSON = '''
+{
+  "status": "ENDED",
+  "statusLock": true,
+  "title": "string",
+  "titleLock": true,
+  "titleSort": "string",
+  "titleSortLock": true,
+  "summary": "string",
+  "summaryLock": true,
+  "publisher": "string",
+  "publisherLock": true,
+  "readingDirectionLock": true,
+  "ageRatingLock": true,
+  "language": "string",
+  "languageLock": true,
+  "genresLock": true,
+  "tagsLock": true,
+  "totalBookCountLock": true,
+  "sharingLabelsLock": true,
+  "tags": [
+    "string"
+  ],
+  "readingDirection": "LEFT_TO_RIGHT",
+  "ageRating": 0,
+  "genres": [
+    "string"
+  ],
+  "totalBookCount": 0,
+  "sharingLabels": [
+    "string"
+  ]
+}
+'''
